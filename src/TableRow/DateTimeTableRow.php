8b89ae8509627028b7729825e26261434e43e1fe
Paul Water <p.r.water@setbased.nl>
2016-05-06 16:29:37 +0200

    Improved adding rows.

--- src/TableRow/DateTimeTableRow.php
+++ src/TableRow/DateTimeTableRow.php
@@ -23,42 +23,29 @@ class DateTimeTableRow
   /**
    * Adds a row with a datetime value to a detail table.
    *
-   * @param DetailTable $table     The (detail) table.
-   * @param string      $header    The row header text.
-   * @param string      $value     The datetime in Y-m-d H:i:s format.
-   * @param string|null $format    The format specifier for formatting the content of this table column. If null
-   *                               the default format is used.
+   * @param DetailTable $table  The detail table.
+   * @param int|string  $header The row header text or word ID.
+   * @param string      $value  The datetime in Y-m-d H:i:s format.
+   * @param string|null $format The format specifier for formatting the content of this table column. If null
+   *                            the default format is used.
    */
   public static function addRow($table, $header, $value, $format = null)
   {
-    $row = '<tr>';
-
-    $row .= '<th>';
-    $row .= Html::txt2Html($header);
-    $row .= '</th>';
-
-    $date = \DateTime::createFromFormat('Y-m-d', $value);
+    $date = \DateTime::createFromFormat('Y-m-d H:i:s', $value);
 
     if ($date)
     {
-      // The $value is a valid date.
-      $row .= '<td class="date" data-value="';
-      $row .= $date->format('Y-m-d');
-      $row .= '">';
-      $row .= Html::txt2Html($date->format(($format) ? $format : self::$defaultFormat));
-      $row .= '</td>';
+      // The $value is a valid datetime.      
+      $table->addRow($header,
+                     ['class'      => 'date',
+                      'data-value' => $date->format('Y-m-d H:i:s')],
+                     $date->format(($format) ? $format : self::$defaultFormat));
     }
     else
     {
       // The $value is not a valid datetime.
-      $row .= '<td>';
-      $row .= Html::txt2Html($value);
-      $row .= '</td>';
+      $table->addRow($header, [], $value);
     }
-
-    $row .= '</tr>';
-
-    $table->addRow($row);
   }
 
   //--------------------------------------------------------------------------------------------------------------------
--- src/TableRow/Ipv4TableRow.php
+++ src/TableRow/Ipv4TableRow.php
@@ -2,7 +2,6 @@
 //----------------------------------------------------------------------------------------------------------------------
 namespace SetBased\Abc\Table\TableRow;
 
-use SetBased\Abc\Helper\Html;
 use SetBased\Abc\Table\DetailTable;
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -15,19 +14,13 @@ class Ipv4TableRow
   /**
    * Adds a row with a IPv4 value to a detail table.
    *
-   * @param DetailTable $table      The (detail) table.
-   * @param string      $header     The row header text.
+   * @param DetailTable $table      The detail table.
+   * @param int|string  $header     The row header text or word ID.
    * @param string      $ip4Address The IPv4 address.
    */
   public static function addRow($table, $header, $ip4Address)
   {
-    $row = '<tr><th>';
-    $row .= Html::txt2Html($header);
-    $row .= '</th><td class="ipv4">';
-    $row .= Html::txt2Html($ip4Address);
-    $row .= '</td></tr>';
-
-    $table->addRow($row);
+    $table->addRow($header, ['class' => 'ipv4'], $ip4Address);
   }
 
   //--------------------------------------------------------------------------------------------------------------------
